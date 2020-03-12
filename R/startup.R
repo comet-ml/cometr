bbaf46d4f46c220a98eb36a02ca61b4a02a7f053
Dean Attali <daattali@gmail.com>
2020-03-12 15:34:56 -0400

    add functions to retrieve startup info

--- R/startup.R
+++ R/startup.R
@@ -17,3 +17,50 @@ get_startup_args <- function() {
   args <- paste(args, collapse = " ")
   args
 }
+
+get_startup_packages <- function() {
+  packages <- installed.packages(noCache = TRUE)
+  packages <- as.data.frame(packages, stringsAsFactors = FALSE)
+  rownames(packages) <- NULL
+  packages <- packages[, c("Package", "Version")]
+  packages
+}
+
+get_startup_executable <- function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  if (length(args) >= 1) {
+    args[1]
+  } else {
+    ""
+  }
+}
+
+get_startup_hostname <- function() {
+  hostname <- R.utils::getHostname.System()
+  if (length(hostname) >= 1) {
+    hostname[[1]]
+  } else {
+    ""
+  }
+}
+
+get_startup_pid <- function() {
+  Sys.getpid()
+}
+
+get_startup_rVersion <- function() {
+  as.character(getRversion())
+}
+
+get_startup_rVersionVerbose <- function() {
+  R.version.string
+}
+
+get_startup_user <- function() {
+  user <- R.utils::getUsername.System()
+  if (length(user) >= 1) {
+    user[[1]]
+  } else {
+    ""
+  }
+}
