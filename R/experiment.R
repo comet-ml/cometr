8e78c5ea5fa907a5407a536455bcf7c70aa020fc
Dean Attali <daattali@gmail.com>
2020-03-13 11:16:16 -0400

    still trying to make cran checks happy

--- R/experiment.R
+++ R/experiment.R
@@ -1,8 +1,8 @@
 #' Create a new experiment
-#' @param project_name
-#' @param workspace
-#' @param api_key
-#' @param log_errors
+#' @param project_name Project name
+#' @param workspace Workspace name
+#' @param api_key API key
+#' @param log_errors Whether or not to log errors
 #' @export
 experiment <- function(
   project_name = NULL, workspace = NULL, api_key = NULL, log_errors = FALSE
