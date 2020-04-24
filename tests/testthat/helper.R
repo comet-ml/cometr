d214dbe75102dcd31b30f61d9bbd1d9117b2e3db
Doug Blank <doug.blank@gmail.com>
2020-04-24 12:42:20 -0400

    Revert "Working get_experiment() with tests"
    This reverts commit 3d84f0aced8b42d015e7f08f35e7a2eea81be8b2.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -22,13 +22,8 @@ cleanup <- function() {
 mock_experiment_by_id <- function(experiment_key, keep_active = FALSE,
                                   log_output = FALSE, log_error = FALSE) {
   .cometrenv$cancreate <- TRUE
-  with_mock(
-    `cometr:::log_other` = function(experiment_key, api_key, key, value) {
-      # Keep new experiments from logging "Created by", "cometr"
-    },
-    Experiment$new(experiment_key = experiment_key, api_key = test_api_key,
-                   keep_active = keep_active, log_output = log_output, log_error = log_error)
-  )
+  Experiment$new(experiment_key = experiment_key, api_key = test_api_key,
+                 keep_active = keep_active, log_output = log_output, log_error = log_error)
 }
 
 mock_experiment_full <- function(experiment_key = generate_random_id(), keep_active = FALSE,
@@ -37,14 +32,9 @@ mock_experiment_full <- function(experiment_key = generate_random_id(), keep_act
   link <- paste0("https://www.comet.ml/", test_ws, "/", test_proj, "/", experiment_key)
   with_mock(
     `cometr:::new_experiment` = function(...) list(experimentKey = experiment_key, link = link), {
-      with_mock(
-	`cometr:::log_other` = function(experiment_key, api_key, key, value) {
-          # Keep new experiments from logging "Created by", "cometr"
-	},
-	create_experiment(api_key = test_api_key, keep_active = keep_active, log_output = log_output,
-			  log_error = log_error, log_code = log_code,
-			  log_system_details = log_system_details, log_git_info = log_git_info)
-      )
+      create_experiment(api_key = test_api_key, keep_active = keep_active, log_output = log_output,
+                        log_error = log_error, log_code = log_code,
+                        log_system_details = log_system_details, log_git_info = log_git_info)
     }
   )
 }
