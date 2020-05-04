fe12051d9a45b0ba57a5bca148c282ed0d5c48e7
Doug Blank <doug.blank@gmail.com>
2020-05-04 09:21:01 -0400

    Apparently a @title and @description were required

--- R/experiment.R
+++ R/experiment.R
@@ -60,6 +60,8 @@ create_experiment <- function(
     log_code = log_code, log_system_details = log_system_details, log_git_info = log_git_info)
 }
 
+#' @title get_experiment
+#' @description
 #' Get a previously created experiment
 #'
 #' Get a previously created experiment on Comet's servers. The return value is an [`Experiment`]
